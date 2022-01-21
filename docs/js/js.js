18fa89208afe2831dba7d6c2e648d2a905effc2e
Pete Pearl <zhemchugov.p@gmail.com>
2022-01-21 15:15:08 +0300

    add background

--- docs/js/js.js
+++ docs/js/js.js
@@ -4,6 +4,7 @@ var root = document.documentElement;
 var app = new Vue({
   el: '#app',
   data: {
+    background: '#FFFFFF',
     color1: '#F519B7',
     color2: '#0085DE',
     color3: '#00D08E',
@@ -15,6 +16,7 @@ var app = new Vue({
       root.style.setProperty('--color2', this.color2);
       root.style.setProperty('--color3', this.color3);
       root.style.setProperty('--color4', this.color4);
+      root.style.setProperty('--background', this.background);
     }
   },
   beforeMount: function beforeMount() {
